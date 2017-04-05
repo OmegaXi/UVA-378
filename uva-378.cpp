4a09caeb09f1a694664111858fdd014623536271
Yestin <641010761@qq.com>
2017-04-05 16:04:59 +0800

    0
    wrong answer
    不知道为什么是错的┑(￣Д ￣)┍

--- uva-378.cpp
+++ uva-378.cpp
@@ -5,32 +5,68 @@
 
 using namespace std;
 
-main()
+int main()
 {
 	int n;
 	int x1,y1,x2,y2,x3,y3,x4,y4;
 	int k1,k2;//斜率判断是否平行
 	int b1,b2;//截距判断是否同一直线
 	double xx,yy;
-	cin>>n;
-	printf("INTERSECTING LINES OUTPUT\n");
-	while(n--)
+	while(cin>>n)
 	{
-		cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
-		k1=(y1-y2)/(x1-x2);
-		b1=y1-k1*x1;
-		k2=(y3-y4)/(x3-x4);
-		b2=y3-k2*x3;
-		if(k1==k2&&b1==b2)
-			printf("LINE\n");
-		else if(k1==k2&&b1!=b2)
-			printf("NONE\n");
-		else
+		printf("INTERSECTING LINES OUTPUT\n");
+		while(n--)
 		{
-			xx=(b2-b1)/(k1-k2);
-			yy=k1*xx+b1;
-			printf("POINT %.2f %.2f\n",xx,yy);
+			cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
+			if((x1-x2)!=0&&(x3-x4)!=0)
+			{
+				k1=(y1-y2)/(x1-x2);
+				b1=y1-k1*x1;
+				k2=(y3-y4)/(x3-x4);
+				b2=y3-k2*x3;
+				if(k1==k2&&b1==b2)
+					printf("LINE\n");
+				else if(k1==k2&&b1!=b2)
+					printf("NONE\n");
+				else
+				{
+					xx=(b2-b1)/(k1-k2);
+					yy=k1*xx+b1;
+					printf("POINT %.2f %.2f\n",xx,yy);
+				}
+			}
+			else if((x1-x2)==0)
+			{
+				if(((x3-x4)==0)&&x1==x3)
+					printf("LINE\n");
+				else if(((x3-x4)==0)&&x1!=x3)
+					printf("NONE\n");
+				else
+				{
+					k2=(y3-y4)/(x3-x4);
+					b2=y3-k2*x3;
+					xx=x1;
+					yy=k2*xx+b2;
+					printf("POINT %.2f %.2f\n",xx,yy);
+				}
+			}
+			else
+			{
+				if(((x1-y2)==0)&&x1==x3)
+					printf("LINE\n");
+				else if(((x1-x2)==0)&&x1!=x3)
+					printf("NONE\n");
+				else
+				{
+					k1=(y1-y2)/(x1-x2);
+					b1=y1-k1*x1;
+					xx=x3;
+					yy=k1*xx+b1;
+					printf("POINT %.2f %.2f\n",xx,yy);
+				}
+			}
 		}
+		printf("END OF OUTPUT\n");
 	}
-	 printf("END OF OUTPUT\n");
+	return 0;
 }
